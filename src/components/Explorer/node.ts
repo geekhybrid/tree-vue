258f4ee5d88e6b9e961f260f463318cdf8457a8f
ikeohachidi <ikeohachidi@gmail.com>
2021-05-28 15:37:19 +0100

    chore: remove unused code

--- src/components/Explorer/node.ts
+++
@@ -1,8 +0,0 @@
-import { Vue, Component, Prop } from 'vue-property-decorator';
-
-import { ExplorerItem, ItemTypes } from '@/contracts/types';
-
-@Component
-export default class Node extends Vue {
-    @Prop({ default: () => { return {} }, required: true }) item!: ExplorerItem;
-}
